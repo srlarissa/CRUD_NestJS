40b686c79c03cea860adf7b6bd484706755cea34
Larissa Soares Reis <larissasoaresreis@gmail.com>
2024-09-11 13:03:31 -0300

    feat: CRUD entry points created

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ApplicationsModule } from './applications/applications.module';
 
 @Module({
-  imports: [],
+  imports: [ApplicationsModule],
   controllers: [AppController],
   providers: [AppService],
 })
